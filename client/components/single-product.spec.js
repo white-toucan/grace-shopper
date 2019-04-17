0a7dcda3a64445a94737564be6962a1d62532eaa
Anna Mai <annam0403@gmail.com>
2019-04-16 23:48:31 -0400

    test: Fix singleProduct spec

--- client/components/single-product.spec.js
+++ client/components/single-product.spec.js
@@ -35,14 +35,10 @@ describe('SingleProduct', () => {
 		expect(singleProduct.is('div#product')).to.equal(true);
 	});
 
-	it('renders the product name in h3', () => {
-		expect(
-			singleProduct.contains([
-        <h3 className="product-info-name">Floppy Disk</h3>,
-        <p className="product-price">"5.99"</p>,
-        // <p className="product-desc">Save up to 1.44MB of data on this portable drive!</p>
-			])
-		).to.equal(true);
+	it('renders the product details in the component', () => {
+		expect(singleProduct.find('h3.product-info-name').text()).to.equal('Floppy Disk');
+		expect(singleProduct.find('p.product-price').text()).to.equal('5.99');
+		expect(singleProduct.find('p.product-desc').text()).to.equal('Save up to 1.44MB of data on this portable drive!');
 	});
 
 	it('calls the addToCart function when the `Add to Cart` button is clicked', function() {
