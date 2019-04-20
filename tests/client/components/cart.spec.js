a863e29ca4f271beddb36e05e2714b2262589db4
Anna Mai <annam0403@gmail.com>
2019-04-20 10:55:18 -0400

    chore: Reorganize all tests

--- tests/client/components/cart.spec.js
+++ tests/client/components/cart.spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai';
 import React from 'react';
 import enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {Cart} from './cart';
+import {Cart} from '../../../client/components/cart';
 
 const adapter = new Adapter();
 enzyme.configure({adapter});
--- tests/server/api/products.spec.js
+++ tests/server/api/products.spec.js
@@ -1,7 +1,7 @@
 const {expect} = require('chai');
 const request = require('supertest');
-const db = require('../db');
-const app = require('../index');
+const db = require('../../../server/db');
+const app = require('../../../server');
 const Product = db.model('product');
 
 describe('GET /products/', () => {
