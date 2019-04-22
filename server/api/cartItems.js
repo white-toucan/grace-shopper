910b88d195486663a6ac67ffe1c5b02ac62f20a0
Robertoabr <roberto.brillembourg@gmail.com>
2019-04-22 17:37:06 -0400

    feature_orderHistory_backend

--- server/api/cartItems.js
+++ server/api/cartItems.js
@@ -7,14 +7,13 @@ const Product = require('../db/models/product');
 router.use('/*', (req, res, next) => {
 	if (!req.user) return res.sendStatus(404);
 	next();
-})
+});
 
 router.get('/', async (req, res, next) => {
 	const {cartId} = req.session;
 
 	try {
-		let products = await Cart.findByPk(cartId)
-			.then(cart => cart.getProducts());
+		let products = await Cart.findByPk(cartId).then(cart => cart.getProducts());
 
 		let productsRes = products.map(({id, name, price, imageUrl, cartItem}) => ({
 			id,
@@ -22,7 +21,7 @@ router.get('/', async (req, res, next) => {
 			price,
 			imageUrl,
 			quantity: cartItem.quantity
-		}))
+		}));
 		res.json(productsRes || []);
 	} catch (error) {
 		next(error);
@@ -37,7 +36,8 @@ router.post('/:productId', async (req, res, next) => {
 	try {
 		let [cartItem, created] = await CartItem.findOrCreate({
 			where: {
-				cartId, productId
+				cartId,
+				productId
 			},
 			defaults: {
 				cartId,
@@ -62,13 +62,12 @@ router.put('/:productId', async (req, res, next) => {
 	const quantity = +req.body.quantity;
 	try {
 		const entry = await CartItem.findOne({
-			where: { cartId, productId }
+			where: {cartId, productId}
 		});
 		if (entry) {
-			const updated = await entry.update({ quantity });
+			const updated = await entry.update({quantity});
 			res.status(200).json(updated);
-		}
-		else res.sendStatus(404);
+		} else res.sendStatus(404);
 	} catch (error) {
 		next(error);
 	}
@@ -79,7 +78,7 @@ router.delete('/:productId', async (req, res, next) => {
 	const {cartId} = req.session;
 	const productId = +req.params.productId;
 	try {
-		await CartItem.destroy({ where: { cartId, productId } });
+		await CartItem.destroy({where: {cartId, productId}});
 		res.sendStatus(204);
 	} catch (error) {
 		next(error);
--- server/api/me.js
+++ server/api/me.js
@@ -1,24 +1,25 @@
 const router = require('express').Router();
 const Cart = require('../db/models/cart');
 const CartItem = require('../db/models/cartItem');
+const Product = require('../db/models/product');
 
 module.exports = router;
 
-router.get('/cart', async(req, res, next) => {
-  try {
-    if (req.user) {
-      const userId = req.user.id;
-      let [cart] = await Cart.findOrCreate({where: { userId, isActive: true } });
-      // Update cart in session with information about latest active cart
-      req.session.cartId = cart.id;
-      res.json(cart);
-    // TODO: guest path
-    } else {
-      res.sendStatus(404);
-    }
-  } catch (error) {
-    next(error);
-  }
+router.get('/cart', async (req, res, next) => {
+	try {
+		if (req.user) {
+			const userId = req.user.id;
+			let [cart] = await Cart.findOrCreate({where: {userId, isActive: true}});
+			// Update cart in session with information about latest active cart
+			req.session.cartId = cart.id;
+			res.json(cart);
+			// TODO: guest path
+		} else {
+			res.sendStatus(404);
+		}
+	} catch (error) {
+		next(error);
+	}
 });
 
 // Checkout cart
@@ -33,23 +34,68 @@ router.put('/cart', async (req, res, next) => {
 			let products = await cart.getProducts();
 
 			// "Freeze" current prices for cartItems in cart
-			Promise.all(products.map(product => {
-				return CartItem.update({purchasePrice: product.price},
-					{where: {productId: product.id, cartId }
-				});
-			}));
+			Promise.all(
+				products.map(product => {
+					return CartItem.update(
+						{purchasePrice: product.price},
+						{
+							where: {productId: product.id, cartId}
+						}
+					);
+				})
+			);
 
 			// "Archive" current cart, effectively creating an order history entry
-			await Cart.update({isActive: false}, {where: {id: cartId, isActive: true} });
+			await Cart.update(
+				{isActive: false},
+				{where: {id: cartId, isActive: true}}
+			);
 
 			// Create new cart
-			let newCart = await Cart.create({ userId });
+			let newCart = await Cart.create({userId});
 			req.session.cartId = newCart.id; // Set new cartId on session
 			res.json(newCart);
 		}
-
 	} catch (error) {
 		next(error);
 	}
+});
 
+//get the historical cartIds for the current user (order history)
+router.get('/orders', async (req, res, next) => {
+	try {
+		if (req.user) {
+			const userId = req.user.id;
+			let cartIdArray = await Cart.findAll({
+				attributes: ['id'],
+				where: {userId, isActive: false}
+			});
+			res.json(cartIdArray);
+		} else {
+			res.sendStatus(404);
+		}
+	} catch (error) {
+		next(error);
+	}
+});
+
+//get a specific non active cart's products and prices (order history)
+router.get('/orders/:cartId', async (req, res, next) => {
+	let cartId = +req.params.cartId;
+	try {
+		if (req.user) {
+			const userId = req.user.id;
+
+			let cartItems = await Cart.findAll({
+				include: [{model: Product}],
+				where: {id: cartId}
+			});
+
+			res.json({cartItems});
+		} else {
+			res.sendStatus(404);
+		}
+	} catch (error) {
+		next(error);
+	}
 });
